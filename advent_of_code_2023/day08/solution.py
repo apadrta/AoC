0b361004d5db39cda71311bf22511764e69b3fd9
apadrta <apadrta@cesnet.cz>
2023-12-08 10:09:23 +0100

    Solution Aoc2023/Day08 added.

---
+++ advent_of_code_2023/day08/solution.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Advent of code 2023 - Day 8
+"""
+
+import argparse
+from math import lcm
+
+
+def get_args():
+    """
+    Cmd line argument parsing (preprocessing)
+    """
+    # Assign description to the help doc
+    parser = argparse.ArgumentParser(
+        description='Advent of code 2023: Day 8'
+        )
+
+    # Add arguments
+    parser.add_argument(
+        '-i',
+        '--infile',
+        type=str,
+        help='Inputfilename',
+        required=True
+        )
+
+    # Array for all arguments passed to script
+    args = parser.parse_args()
+
+    # Return arg variables
+    return args.infile
+
+
+def read_data_struct(filename):
+    """
+    Read data from file and convert it to data structure
+    """
+    data = []
+    with open(filename, "r", encoding="utf-8") as fileh:
+        data = fileh.readlines()
+    data = [x.strip() for x in data]
+    # prepare directions manual (0 left, 1 right)
+    directions = [int(x) for x in data[0].replace('L', '0').replace('R', '1')]
+    maps = {}
+    for line in data[2:]:
+        src, dst = line.split(' = ')
+        maps[src] = dst[1:-1].split(', ')
+    return directions, maps
+
+
+def move_to_zzz(dirs, maps, src='AAA', dst='ZZZ'):
+    """
+    Move from AAA to ZZZ
+    """
+    pos = src
+    inst = 0
+    steps = 0
+    while pos != dst:
+        pos = maps[pos][dirs[inst]]
+        steps += 1
+        inst = (inst + 1) % len(dirs)
+    return steps
+
+
+def move_to_z(dirs, maps, src='AAA'):
+    """
+    Move from AAA to ZZZ
+    """
+    pos = src
+    inst = 0
+    steps = 0
+    while pos[2] != 'Z':
+        pos = maps[pos][dirs[inst]]
+        steps += 1
+        inst = (inst + 1) % len(dirs)
+    return steps
+
+
+def move_to_zzzs(dirs, maps):
+    """
+    MOVE from .*A to (.*Z) in optimal way
+    """
+    pos = []
+    for point in maps:
+        if point[2] == 'A':
+            pos.append(point)
+    lens = []
+    for point in pos:
+        lens.append(move_to_z(dirs, maps, point))
+    res = 1
+    for num in lens:
+        res = lcm(res, num)
+    return res
+
+
+def main():
+    """
+    Main function
+    """
+
+    # process args
+    infile = get_args()
+
+    # read data
+    dirs, maps = read_data_struct(infile)
+
+    # part 1
+    sums = 0
+    sums = move_to_zzz(dirs, maps)
+    print(f"Part 1 solution: {sums}")
+
+    # part 2
+    sums = move_to_zzzs(dirs, maps)
+    print(f"Part 2 solution: {sums}")
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
